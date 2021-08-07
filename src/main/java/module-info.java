0d42a6a8c3d182e54f8058b77d512c3cb5c8756a
Jinzhaolu <jinzhaolunesp@163.com>
2021-08-07 21:41:14 +0800

    Update: update nesp-sdk-java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.nesp.sdk.javafx {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.jetbrains.annotations;
-
+    requires java.base;
+    requires com.nesp.sdk.java;
 
     opens com.nesp.sdk.javafx to javafx.fxml;
     exports com.nesp.sdk.javafx;
