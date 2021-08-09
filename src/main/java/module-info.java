76b46b6bbf509d4656f72d44a4100ff8b475333b
靳兆鲁 <1756404649@qq.com>
2021-08-10 02:35:11 +0800

    Add: Rect and RectF

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.nesp.sdk.javafx {
-    requires javafx.controls;
+    requires javafx.base;
     requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
     requires org.jetbrains.annotations;
     requires java.base;
+    requires java.desktop;
+    requires commons.math3;
     requires com.nesp.sdk.java;
 
     opens com.nesp.sdk.javafx to javafx.fxml;
